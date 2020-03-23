c79d5d3804b5010ecb74392cb8ded0298aec415f
Derek Evanson <evanson.derek@gmail.com>
2020-03-22 22:48:18 -0500

    Get request to local host port  3000, Postman pass

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // JavaScript written by Derek Evanson with the guided process of Jonas Schmedtmann as part of "Node.js BootCamp 2020"
 // HTML and CSS provided by Jonas Schmedtmann
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from the server side!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
