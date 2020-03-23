886d98d185eff10248498598c9d8f864ccb92d1f
Derek Evanson <evanson.derek@gmail.com>
2020-03-23 01:07:32 -0500

    Can mod json with post

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
 // JavaScript written by Derek Evanson with the guided process of Jonas Schmedtmann as part of "Node.js BootCamp 2020"
-// HTML and CSS provided by Jonas Schmedtmann
-
+// HTML, CSS, and JSON provided by Jonas Schmedtmann
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side!');
+app.use(express.json()); // Middleware
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 const port = 3000;
