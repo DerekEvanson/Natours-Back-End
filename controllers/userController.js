3d0fc2c845c5786a07f0399b4266c0c1dff01f86
Derek Evanson <evanson.derek@gmail.com>
2020-03-25 22:33:08 -0500

    Setting up for mongoDB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,39 +1,9 @@
-// (Place holder)
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
+exports.getAllUsers = (req, res) => {};
 
-// (Place holder)
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
+exports.getUser = (req, res) => {};
 
-// (Place holder)
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
+exports.createUser = (req, res) => {};
 
-// (Place holder)
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
+exports.updateUser = (req, res) => {};
 
-// (Place holder)
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
+exports.deleteUser = (req, res) => {};
