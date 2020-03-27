08a4d058420de7bc75f49c1df0f4e022d6755178
Derek Evanson <evanson.derek@gmail.com>
2020-03-26 23:07:05 -0500

    Field Limiting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,6 +23,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // Execute query
     const tours = await query;
 
