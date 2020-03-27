4a3543108b8f434a2bf0bbbd8f6ca6d79581af3c
Derek Evanson <evanson.derek@gmail.com>
2020-03-26 22:50:49 -0500

    Sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,7 +13,15 @@ exports.getAllTours = async (req, res) => {
     // Filter for value ranges
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // Execute query
     const tours = await query;
