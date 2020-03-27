4ccdda30f428fd116ae692733231fdd09bfe7a96
Derek Evanson <evanson.derek@gmail.com>
2020-03-26 23:28:53 -0500

    Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,6 +31,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // Execute query
     const tours = await query;
 
