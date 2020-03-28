6d78343d0b7b514441e5237bb7805374a5cdcc6e
Derek Evanson <evanson.derek@gmail.com>
2020-03-28 17:31:50 -0500

    Refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = async (req, res, next) => {
   req.query.limit = '5';
@@ -8,49 +9,16 @@ exports.aliasTopTours = async (req, res, next) => {
 };
 
 exports.getAllTours = async (req, res) => {
-  //console.log(queryObj);
   try {
-    // Filter results excluding non filter fields
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'fields', 'limit'];
-    excludedFields.forEach((e) => delete queryObj[e]);
-
-    // Filter for value ranges
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     // Execute query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
+    // Send response
     res.status(200).json({
       status: 'success',
       results: tours.length,
