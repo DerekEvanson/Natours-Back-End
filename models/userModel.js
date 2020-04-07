b5b99bad14e764f468b3bba9888a22245e7836d6
Derek Evanson <evanson.derek@gmail.com>
2020-04-07 16:36:07 -0500

    User model

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,14 +19,33 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have a password'],
     maxlength: [60, 'A name must have less or equal then 10 characters'],
-    minlength: [10, 'A name must have more or equal then 10 characters'],
+    minlength: [2, 'A name must have more or equal then 2 characters'],
   },
   passwordConfirm: {
     type: String,
     required: [true, 'A user must have a password'],
     maxlength: [60, 'A name must have less or equal then 10 characters'],
-    minlength: [10, 'A name must have more or equal then 10 characters'],
+    minlength: [2, 'A name must have more or equal then 2 characters'],
+    validate: {
+      // This only works on User.create and User.save
+      validator: function (e) {
+        return e === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = new mongoose.model('User', userSchema);
