0e9879977bf3c18f0e9ac329cd0f238eb07b92d1
Derek Evanson <evanson.derek@gmail.com>
2020-04-09 23:49:12 -0500

    Nested Routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,7 @@ const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 const express = require('express');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
