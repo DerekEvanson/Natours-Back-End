6ac2d5b42faf85b51c13eaefb62c67132dec75e8
Derek Evanson <evanson.derek@gmail.com>
2020-05-07 07:32:43 -0500

    Heroku Config

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// process.on('uncaughtException', (err) => {
-//   console.log('UNCAUGHT EXCEPTION... Shutting down...');
-//   console.log(err.name, err.message);
-//   process.exit(1);
-// });
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION... Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 dotenv.config({ path: './newConfig.env' });
 const app = require('./app');
@@ -29,10 +29,17 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-// process.on('unhandledRejection', (err) => {
-//   console.log('UNHANDLED REJECTION... Shutting down...');
-//   console.log(err.name, err.message);
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION... Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('Process Stopped');
+  });
+});
