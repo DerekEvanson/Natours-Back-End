df982bb77c9895e0a39c41e88680471244a43187
Derek Evanson <evanson.derek@gmail.com>
2020-05-07 07:54:31 -0500

    Cors

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 
+const cors = require('cors');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -26,6 +28,18 @@ app.set('views', path.join(__dirname, 'views'));
 
 // GLOBAL MiddleWear
 //====================================================
+
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
